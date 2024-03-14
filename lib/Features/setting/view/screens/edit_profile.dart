c345e97efd08ef147d75c85277562e412521d1d9
ahmed yassin <83839308+79958ahmed@users.noreply.github.com>
2024-03-14 17:52:56 +0200

    finish edit profile UI

--- lib/Features/setting/view/screens/edit_profile.dart
+++ lib/Features/setting/view/screens/edit_profile.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:sell_4_u/core/helper/component/component.dart';
 class EditProfile extends StatelessWidget {
+
   const EditProfile({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var nameController = TextEditingController();
+    var passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -16,167 +23,216 @@ class EditProfile extends StatelessWidget {
           IconButton(onPressed: (){}, icon: const Icon(Icons.power_settings_new_sharp,color: Colors.blue,size: 28,))
         ],
       ),
-      body: Column(
-        children:
-        [
-          Stack(
-            children:[
-              Container(
-              width: double.infinity,
-              height: 200,
-              color: Colors.blue,
-              child: Align(
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children:
-                  [
-                    Stack(
-                      children:[
-                        Stack(
-                          children: [
-                            const CircleAvatar(
-                              radius: 60,
-                            ),
-                            IconButton(onPressed: (){}, icon:const Icon(Icons.mode_edit_outline_sharp))
-
-                          ],
-                        ),
-
-                          ],
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          children:
+          [
+            Stack(
+              children:[
+                Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.blue,
+                child: Align(
+                  child: Stack(
+                    alignment: AlignmentDirectional.center,
+                    children:
+                    [
+                      Stack(
+                        children:[
+                          Stack(
+                            children: [
+                              const CircleAvatar(
+                                radius: 60,
+                              ),
+                              IconButton(onPressed: (){}, icon:const Icon(Icons.mode_edit_outline_sharp))
+        
+                            ],
+                          ),
+        
+                            ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-      ],
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 70,vertical: 10),
-            child: Row(
-              children:
-              [
-                Column(
-                  children:
-                  [
-                    Text('1'),
-                    Text('Followers'),
-                  ],
-                ),
-               Spacer(),
-                Column(
-                  children:
-                  [
-                    Text('0'),
-                    Text('Following'),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              children:
-              [
-                Container(
-                  height: 65,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                        style: BorderStyle.solid),
-                  ),
-                  child: const Row(
-                    children:
-                    [
-                      Icon(Icons.phone_android_rounded,size: 28,),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:
-                        [
-                          Text('Mobile',),
-                          Text('+54545152'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 65,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                        style: BorderStyle.solid),
-                  ),
-                  child: const Row(
-                    children:
-                    [
-                      Icon(Icons.wallet,size: 28,),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:
-                        [
-                          Text('My wallet',),
-                          Row(
-                            children: [
-                              Text('Free credit:'),
-                              Text(' 3.0'),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text('Premium credit:'),
-                              Text(' 0.0'),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 65,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                        style: BorderStyle.solid),
-                  ),
-                  child: const Row(
-                    children:
-                    [
-                      Icon(Icons.verified_user_sharp,size: 28,),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Row(
-                        children: [
-                          Text('Member since:'),
-                          Text(' 04 Dec 2020'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
         ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 70,vertical: 10),
+              child: Row(
+                children:
+                [
+                  Column(
+                    children:
+                    [
+                      Text('1'),
+                      Text('Followers'),
+                    ],
+                  ),
+                 Spacer(),
+                  Column(
+                    children:
+                    [
+                      Text('0'),
+                      Text('Following'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children:
+                [
+                  Container(
+                    height: 65,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                          style: BorderStyle.solid),
+                    ),
+                    child: const Row(
+                      children:
+                      [
+                        Icon(Icons.phone_android_rounded,size: 28,),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:
+                          [
+                            Text('Mobile',),
+                            Text('+54545152'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 65,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                          style: BorderStyle.solid),
+                    ),
+                    child: const Row(
+                      children:
+                      [
+                        Icon(Icons.wallet,size: 28,),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children:
+                          [
+                            Text('My wallet',),
+                            Row(
+                              children: [
+                                Text('Free credit:'),
+                                Text(' 3.0'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text('Premium credit:'),
+                                Text(' 0.0'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 65,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                          style: BorderStyle.solid),
+                    ),
+                    child: const Row(
+                      children:
+                      [
+                        Icon(Icons.verified_user_sharp,size: 28,),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Row(
+                          children: [
+                            Text('Member since:'),
+                            Text(' 04 Dec 2020'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Align(
+                  alignment: AlignmentDirectional.topStart,
+                  child: Text('More information',style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),)),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+        TextFormField(
+          keyboardType: TextInputType.name,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.person),
+            labelText: 'Name',
+            border: OutlineInputBorder(),
+          ),
+        ),
+            SizedBox(
+              height: 13,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(
+              height: 13,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+      SizedBox(
+        height: 13,
+      ),
+        Buttonhelper(title: 'Save',onchange: (){},),
+
+          ],
+        ),
       ),
     );
   }
